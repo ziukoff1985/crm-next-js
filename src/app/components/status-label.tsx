774182a43c0e4a053eca355caba3b4e8222cbec2
Borys Ziukov <ziukoff1985@gmail.com>
2025-05-22 15:48:07 +0300

    Add clsx, Rework status label using clsx, Add disabled prop to status label

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 
 export enum Status {
   Active = 'active',
@@ -9,18 +10,24 @@ export enum Status {
 export interface StatusLabelProps {
   children: React.ReactNode;
   status?: Status;
+  disabled?: boolean;
 }
 
-export default function StatusLabel({ children, status }: StatusLabelProps) {
+export default function StatusLabel({
+  children,
+  status,
+  disabled,
+}: StatusLabelProps) {
   return (
     <div
-      className={`inline-flex items-center rounded-3xl px-3.5 py-1 text-sm font-medium ${
-        (status === Status.Active && 'bg-green-100 text-green-700') ||
-        (status === Status.NotActive && 'bg-red-100 text-red-700') ||
-        (status === Status.Pending && 'bg-orange-100 text-orange-700') ||
-        (status === Status.Suspended && 'bg-blue-100 text-blue-700') ||
-        ''
-      }`}
+      className={clsx(
+        'inline-flex items-center rounded-3xl px-3.5 py-1 text-sm font-medium',
+        status === Status.Active && 'bg-green-100 text-green-700',
+        status === Status.NotActive && 'bg-red-100 text-red-700',
+        status === Status.Pending && 'bg-orange-100 text-orange-700',
+        status === Status.Suspended && 'bg-blue-100 text-blue-700',
+        { ['cursor-not-allowed opacity-50']: disabled }
+      )}
     >
       <div className="mr-2 h-1 w-1 rounded-full bg-current"></div>
       {children}
