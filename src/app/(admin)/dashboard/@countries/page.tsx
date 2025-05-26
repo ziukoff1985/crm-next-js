ad9f29b443fa07e07b65583dd3ae977720f9c47f
Borys Ziukov <ziukoff1985@gmail.com>
2025-05-26 12:06:13 +0300

    Data fetching: Add using fetch

--- src/app/(admin)/dashboard/@countries/page.tsx
+++ src/app/(admin)/dashboard/@countries/page.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
 import clsx from 'clsx';
-import { getSummaryCountries } from '@/lib/api';
+import { getCompanies, getCountries } from '@/lib/api';
 import DashboardCard from '@/app/components/dashboard-card';
+import getCountById from '@/lib/utils/getCountById';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface PageProps {}
 
 export default async function Page({}: PageProps) {
-  const data = await getSummaryCountries();
+  const countries = await getCountries();
+  const companies = await getCompanies();
+
+  const counts = getCountById(companies, 'countryId');
 
   return (
     <DashboardCard label="Countries of companies">
       <div className="flex items-end gap-2 px-5 pb-5">
         <div>
-          {data.map(({ countryId, countryTitle, count }) => (
+          {countries.map(({ id, title }) => (
             <p
-              key={countryId}
+              key={id}
               className={clsx(
                 'text-sm font-medium text-gray-900',
                 'before:mr-2 before:inline-block before:h-2 before:w-2 before:rounded-full before:bg-purple-200 before:align-middle'
               )}
-            >{`${countryTitle} - ${count}`}</p>
+            >{`${title} - ${counts[id] || 0}`}</p>
           ))}
         </div>
         <Image width={395} height={262} src="/images/world.svg" alt="world" />
--- src/app/(admin)/dashboard/@stats/page.tsx
+++ src/app/(admin)/dashboard/@stats/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { getSummaryStats } from '@/lib/api';
+import { getSummaryStats, SummaryStats } from '@/lib/api';
 import StatCard, { StatCardType } from '@/app/components/stat-card/stat-card';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface PageProps {}
 
-const labelByStat = {
+const labelByStat: Record<keyof SummaryStats, string> = {
   promotions: 'Total promotions',
   categories: 'Total categories',
   newCompanies: 'New companies',
@@ -17,7 +17,7 @@ export default async function Page({}: PageProps) {
 
   return (
     <div className="grid grid-cols-12 gap-5">
-      {(Object.keys(labelByStat) as (keyof typeof data)[]).map((key) => (
+      {(Object.keys(labelByStat) as (keyof SummaryStats)[]).map((key) => (
         <div key={key} className="col-span-3">
           <StatCard
             type={StatCardType.Gradient}
